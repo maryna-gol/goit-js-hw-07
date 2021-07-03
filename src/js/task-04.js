4bc159a95b13782a4926d461f209d84a1e7dbe58
maryna-gol <7victory7@mail.ru>
2021-07-03 22:53:16 +0200

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,21 +1,12 @@
 
+/***/
+let counterValue = document.querySelector('#value');
+let incrementBtn = document.querySelector('[data-action="increment"]');
+let decrementBtn = document.querySelector('[data-action="decrement"]');
 
-let currentValueRef = document.querySelector('#value');
-let value = 0;
-
-const countAdd = document.querySelector('[data-action="increment"]')
-.addEventListener('click', function increment (){
-    value  += 1;    
-    currentValue.textContent = value;    
-  
-    return  currentValueRef;
-});
-
-
-const countSub = document.querySelector('[data-action="decrement"]')
-.addEventListener('click', function decrement (event){    
-    value  -= 1;    
-    currentValue.textContent = value;    
-    
-    return  currentValueRef;
-});
+const incrementFunction = () =>
+  (counterValue.textContent = Number(counterValue.textContent) + 1);
+const decrementFunction = () =>
+  (counterValue.textContent = Number(counterValue.textContent) - 1);
+decrementBtn.addEventListener('click', decrementFunction);
+incrementBtn.addEventListener('click', incrementFunction);
